7fe2963d1d795b6a86e8a764e5cc1f78f7c7bad0
Harsh Giri <2006.harshgiri@gmail.com>
2026-09-03 21:57:30 +0530

    Completed hashing

--- Hashing/30.py
+++ Hashing/30.py
@@ -1,25 +1,27 @@
-# Find the highest/lowest frequency element
+l = [10, 5, 10, 15, 10, 5]
 
-
-# 30
-
-# Problem Statement: Problem Statement: Given an array of size N. Find the highest and lowest frequency element.
-
-
-
-# Input: array[] = {10,5,10,15,10,5};
-# Output: 10 15
-
-
-l = [10,5,10,15,10,5]
 freq = {}
-for i in l :
-    if i == freq[i]:
-        freq[i] +=1
-    else :
+
+for i in l:
+    if i in freq:
+        freq[i] += 1
+    else:
         freq[i] = 1
-    h_freq = 0
-    h_ele = 0
-    l_freq = 0
-    l_ele = 9999
-    
+
+h_freq = 0
+h_ele = 0
+
+l_freq = float('inf')
+l_ele = 0
+
+for i in freq:
+    if freq[i] > h_freq:
+        h_freq = freq[i]
+        h_ele = i
+
+    if freq[i] < l_freq:
+        l_freq = freq[i]
+        l_ele = i
+
+print(h_ele)
+print(l_ele)
